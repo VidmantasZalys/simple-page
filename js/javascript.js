089f95f88ff9900b568632a3d7eea33bfaa4635b
VidmantasZalys <nutonas@gmail.com>
2021-10-03 22:55:38 +0300

    init

--- js/javascript.js
+++ js/javascript.js
@@ -3,8 +3,7 @@ const nav = document.querySelectorAll("nav");
 const banner = document.querySelector(".banner");
 const movingArea = document.querySelector(".moving-area");
 const imgAvatar = document.querySelector(".img-avatar");
-const circle1 = document.querySelector(".circle-1");
-const circle2 = document.querySelector(".circle-2");
+const projects = document.querySelector(".projects");
 //nav
 
 //banner
@@ -14,3 +13,38 @@ banner.addEventListener("mouseout", function (e) {
 banner.addEventListener("mouseover", function (e) {
   imgAvatar.style.clipPath = `circle(45% at 50% 50%)`;
 });
+
+//projects
+projects.addEventListener("mouseover", function (e) {
+  if (e.target.classList.contains("project-img")) {
+    e.target.classList.add("project-img-in-sh");
+    e.target.classList.remove("project-img-out-sh");
+  }
+});
+
+projects.addEventListener("mouseout", function (e) {
+  if (e.target.classList.contains("project-img")) {
+    e.target.classList.remove("project-img-in-sh");
+    e.target.classList.add("project-img-out-sh");
+  }
+});
+
+const projectObserv = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) return;
+  /*   document.querySelectorAll(".row").forEach((row) => {
+    row.classList.remove("projects-ul-hidden");
+  }); */
+  entry.target.classList.remove("projects-ul-hidden");
+  observer.unobserve(entry.target);
+};
+
+const projectsObserver = new IntersectionObserver(projectObserv, {
+  root: null,
+  threshold: 0.75,
+});
+document.querySelectorAll(".row").forEach((row) => {
+  projectsObserver.observe(row);
+});
+// projectsObserver.observe(projects);
